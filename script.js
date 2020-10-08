b3df6ffd254036036a216dec19758312e28403f3
harimukti14 <64297800+harimukti14@users.noreply.github.com>
2020-10-08 16:31:17 +0700

    menambahkan file html dan javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+// menangkap pilihan user
+var i = true;
+while(i){
+    var p = prompt('masukan pilihan kamu: \njempol, telunjuk, kelingking');
+    // menangkap pilihan komp
+    // membangitkan bilangan random
+    var komp = Math.random();
+    if (komp < 0.34){
+        komp = 'jempol';
+    } else if(komp>= 0.34 && komp < 0.67){
+        komp = 'telunjuk';
+    } else {
+        komp = 'kelingking';
+    }
+
+    // menentukan rules
+    var hasil = '';
+    if (p == komp){
+        hasil = 'SERI';
+    }else if(p == 'jempol'){
+    // if (komp == 'telunjuk'){
+    //     hasil = 'MENANG';
+    // }else if(komp == 'kelingking'){
+    //     hasil = 'KALAH';
+    // }
+        hasil = (komp == 'telunjuk') ? 'MENANG' : 'KALAH';
+    }else if(p == 'telunjuk'){
+        hasil = (komp == 'jempol') ? 'KALAH' : 'MENANG';
+    }else if (p == 'kelingking'){
+        hasil= (komp == 'jempol') ? 'MENANG': 'KALAH';
+    }else {
+        hasil = 'SALAH MEMASUKKAN PILIHAN';
+    }
+
+    //tampilkan hasil
+    alert('kamu memilih '+ p + ' dan komputer memilih '+ komp+'\nmaka kamu ' + hasil);
+u
+    i = confirm('Main Lagi?');
+}
+
+alert ('terima kasih sudah bermain');
